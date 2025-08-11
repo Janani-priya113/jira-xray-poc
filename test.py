a2849be2de64e29cc4be78506b15a2be80a154d2
Janani-priya113 <jananipriya0111@gmail.com>
2025-08-11 17:53:30 +0530

    updated

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import os
 
 # Get secret from environment
-secret_value = os.getenv("MY_SECRET")+"sasasasasasasasas"
+xray_client_id = os.getenv("xray_client_id")
+xray_client_secret = os.getenv("xray_client_secret")
+
+projectKey = os.getenv("projectKey")
+issueKey = os.getenv("issueKey")
 versionName = os.getenv("versionName")
+details = os.getenv("details")
+errorMessage = os.getenv("errorMessage")
 status = os.getenv("status")
 
-print("Secret from Jenkins:", secret_value,versionName,status)
+
+print("Xray Client ID:", xray_client_id)
+print("Xray Client Secret:", xray_client_secret)
+print("Project Key:", projectKey)
+print("Issue Key:", issueKey)
+print("Version Name:", versionName)
+print("Details:", details)
+print("Error Message:", errorMessage)
+print("Status:", status)
+
 # Use the secret for API calls, DB connections, etc.
