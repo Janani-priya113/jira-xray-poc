e07649868f2d424a8e04f94593e8dd29d2f9fe09
Janani-priya113 <jananipriya0111@gmail.com>
2025-08-11 17:34:21 +0530

    updated

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 # Get secret from environment
-secret_value = os.getenv("MY_SECRET")
+secret_value = os.getenv("MY_SECRET")+"sasasasasasasasas"
 
 print("Secret from Jenkins:", secret_value)
 # Use the secret for API calls, DB connections, etc.
